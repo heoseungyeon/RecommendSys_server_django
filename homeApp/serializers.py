13728d17a03ff1400486be675144bde5e944e522
LeeJuOh <xop4p@naver.com>
2020-06-06 16:50:35 +0900

    home image, nickname

--- homeApp/serializers.py
+++ homeApp/serializers.py
@@ -10,9 +10,12 @@ class RecommendUserPlaceHistorySerializer(serializers.ModelSerializer):
         fields = ['place_id', 'place_name']
 
 class UserPlaceHistorySerializer(serializers.ModelSerializer):
+
+    nickname = serializers.CharField(required=False, source='user_idx.nickname')
+    image = serializers.ImageField(required=False, source='user_idx.image')
     class Meta:
         model = UserPlaceHistory
-        fields = '__all__'
+        fields = ('idx','image', 'place_id', 'context', 'img_url_1', 'img_url_2', 'img_url_3', 'img_url_4', 'img_url_5', 'date', 'tag_1', 'tag_2', 'tag_3', 'tag_4', 'tag_5', 'tag_6', 'place_name', 'user_idx', 'nickname')
 
 
 class PickPlaceSerializer(serializers.ModelSerializer):
