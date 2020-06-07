935f03935cbd9400f5175744bbf791430c5b6467
heoseungyeon <42290273+heoseungyeon@users.noreply.github.com>
2020-06-07 20:32:45 +0900

    error modify

--- homeApp/models.py
+++ homeApp/models.py
@@ -18,63 +18,63 @@ def date_upload_profile(instance, filename):
         ])
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, user_id, user_email, password=None):
-        if not user_email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(
-            user_id=user_id,
-            user_email=self.normalize_email(user_email)
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, user_id, user_email, password=None):
-        user = self.create_user(
-            user_id=user_id,
-            user_email=self.normalize_email(user_email),
-            password=password
-
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-class User(AbstractBaseUser):
-
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    idx = models.AutoField(primary_key=True)
-    nickname = models.CharField(unique=True,max_length=100, null = True)
-    user_nm = models.CharField(max_length=50, null = True)
-    user_email = models.CharField(unique=True, max_length=100, null = True)
-    posting_cnt = models.IntegerField(blank=True, null=True, default = 0)
-    following_cnt = models.IntegerField(blank=True, null=True, default = 0)
-    follower_cnt = models.IntegerField(blank=True, null=True, default = 0)
-    description = models.TextField(blank=True, null=True)
-    age = models.DateField(blank=True,null = True)
-    sex = models.CharField(max_length=45, blank=True,null = True)
-    image = models.ImageField(upload_to=date_upload_profile, default='default/default.png')
-
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    object = UserManager()
-    USERNAME_FIELD = 'user_email'
-    # REQUIRED_FIELDS = ['user_id']
-
-    class Meta:
-        managed = False
-        db_table = 'user'
+# class UserManager(BaseUserManager):
+#     def create_user(self, user_id, user_email, password=None):
+#         if not user_email:
+#             raise ValueError("Users must have an email address")
+#
+#         user = self.model(
+#             user_id=user_id,
+#             user_email=self.normalize_email(user_email)
+#         )
+#
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, user_id, user_email, password=None):
+#         user = self.create_user(
+#             user_id=user_id,
+#             user_email=self.normalize_email(user_email),
+#             password=password
+#
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+#
+# class User(AbstractBaseUser):
+#
+#     password = models.CharField(max_length=128)
+#     last_login = models.DateTimeField(blank=True, null=True)
+#     idx = models.AutoField(primary_key=True)
+#     nickname = models.CharField(unique=True,max_length=100, null = True)
+#     user_nm = models.CharField(max_length=50, null = True)
+#     user_email = models.CharField(unique=True, max_length=100, null = True)
+#     posting_cnt = models.IntegerField(blank=True, null=True, default = 0)
+#     following_cnt = models.IntegerField(blank=True, null=True, default = 0)
+#     follower_cnt = models.IntegerField(blank=True, null=True, default = 0)
+#     description = models.TextField(blank=True, null=True)
+#     age = models.DateField(blank=True,null = True)
+#     sex = models.CharField(max_length=45, blank=True,null = True)
+#     image = models.ImageField(upload_to=date_upload_profile, default='default/default.png')
+#
+#     is_admin = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     date_joined = models.DateTimeField(auto_now_add=True)
+#
+#     object = UserManager()
+#     USERNAME_FIELD = 'user_email'
+#     # REQUIRED_FIELDS = ['user_id']
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'user'
 
 # Create your models here.
 class UserSearchHistory(models.Model):
     idx = models.AutoField(primary_key=True)
-    user_idx = models.ForeignKey('User', models.DO_NOTHING, db_column='user_idx', blank=True, null=True)
+    user_idx = models.ForeignKey(User, models.DO_NOTHING, db_column='user_idx', blank=True, null=True)
     text = models.CharField(max_length=1000, blank=True, null=True)
     date = models.DateTimeField(blank=True, null=True)
 
@@ -91,4 +91,3 @@ class UserPick(models.Model):
         managed = False
         db_table = 'user_pick'
         unique_together = (('user_idx', 'place_id'),)
-
